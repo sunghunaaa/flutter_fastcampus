81fe313cc9db9869402090668057d3d457aaab40
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2024-03-13 23:52:43 +0900

    widget 상하좌우 배치

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,64 +1,201 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Study to Container'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Widget을 상하로 배치하기'),
+        ),
+        body: Scroll(),
       ),
-      body: CustomContainer(),
     ),
-  ));
+  );
 }
 
-///double.infinity
-///shade (black은 shade가 없음)
-///Hex color 사용 방법 9CDFD7EA 왼쪽 index에서 색깔 뽑아서 0x 붙이고 뒤에 EA 빼서 앞에 붙이면 됨 (결과 : 0xEA9CDFD7)
-///FF9CDFD7 FF는 투명도 9C빨간 DF녹색 D7파란색
-///decoration 사용시 Container만의 조금 특별한 에러 Failed assertion: line 269 pos 15: 'color == null || decoration == null'
-///상위 container에 color가 있으면 decoration에서 에러가 발생함. (Cannot provide both a color and a decoration)
-///해결 방법은 decoration 안에 color를 넣으면 됨
-///
-///
-class CustomContainer extends StatelessWidget {
-  const CustomContainer({super.key});
+class Scroll extends StatelessWidget {
+  const Scroll({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        // width: double.infinity,
-        width: 300,
-        height: 300,
-        padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
-        decoration: BoxDecoration(
-          color: Color(0xEA9CDFD7),
-          border:
-              Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: [
-            BoxShadow(
-              // color: Colors.grey,
-              color: Colors.black.withOpacity(0.3),
-              offset: Offset(6, 6),
-              blurRadius: 10,
-              spreadRadius: 10,
-            ),
-            BoxShadow(
-              // color: Colors.grey,
-              color: Colors.blue.withOpacity(0.3),
-              offset: Offset(-6, -6),
-              blurRadius: 10,
-              spreadRadius: 10,
-            ),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class BodyHV extends StatelessWidget {
+  const BodyHV({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.red,
+              child: Text('Container1'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.green,
+              child: Text('Container2'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.blue,
+              child: Text('Container3'),
+            ),
           ],
         ),
-        margin: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-        // color: Colors.red.shade200,
-        child: Center(
-            child: Container(
-                color: Colors.yellow, child: Text('Hello Container'))),
+        Container(
+          width: 300,
+          height: 120,
+          color: Colors.grey,
+          child: Text('container4'),
+        )
+      ],
+    );
+  }
+}
+
+class BodyHorizen extends StatelessWidget {
+  const BodyHorizen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.grey,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.red,
+            child: Text('Container1'),
+          ),
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.green,
+            child: Text('Container2'),
+          ),
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.blue,
+            child: Text('Container3'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+///Placeholder widget는 해당 위치에 widget이 올 예정이니 사이즈만큼 자리를 미리 차지하고 있는 widget
+///Column은 기본은 처음 생성시 가운데 맨위에 딱 달라붙게 배치됨. 가운데 배치를 원하면 mainAxisAlignment 사용해야 됨
+class BodyVertical extends StatelessWidget {
+  const BodyVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return Placeholder(
+    //   child: Text('Place holder'),
+    // );
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.grey,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.red,
+            child: Text('Container1'),
+          ),
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.green,
+            child: Text('Container2'),
+          ),
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.blue,
+            child: Text('Container3'),
+          ),
+        ],
       ),
     );
   }
