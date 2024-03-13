4e0c60e98b577bf7013baf72ccd6603b93ec02fb
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2024-03-13 22:38:29 +0900

    no message

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,48 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                print('Tab!');
-              },
-            ),
-            Icon(Icons.play_arrow),
-          ],
-          centerTitle: false,
-          title: Text('This is App bar'),
-        ),
-        body: TestWidget(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.bug_report),
-          onPressed: (){
-            print('Tab fAb!');
-          },
-        ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Study to Container'),
       ),
+      body: CustomContainer(),
     ),
-  );
+  ));
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+///double.infinity
+///shade (black은 shade가 없음)
+///Hex color 사용 방법 9CDFD7EA 왼쪽 index에서 색깔 뽑아서 0x 붙이고 뒤에 EA 빼서 앞에 붙이면 됨 (결과 : 0xEA9CDFD7)
+///FF9CDFD7 FF는 투명도 9C빨간 DF녹색 D7파란색
+///decoration 사용시 Container만의 조금 특별한 에러 Failed assertion: line 269 pos 15: 'color == null || decoration == null'
+///상위 container에 color가 있으면 decoration에서 에러가 발생함. (Cannot provide both a color and a decoration)
+///해결 방법은 decoration 안에 color를 넣으면 됨
+///
+///
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Text(
-          'hello, flutter',
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.black,
-          ),
+    return Center(
+      child: Container(
+        // width: double.infinity,
+        width: 300,
+        height: 300,
+        padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+        decoration: BoxDecoration(
+          color: Color(0xEA9CDFD7),
+          border:
+              Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(
+              // color: Colors.grey,
+              color: Colors.black.withOpacity(0.3),
+              offset: Offset(6, 6),
+              blurRadius: 10,
+              spreadRadius: 10,
+            ),
+            BoxShadow(
+              // color: Colors.grey,
+              color: Colors.blue.withOpacity(0.3),
+              offset: Offset(-6, -6),
+              blurRadius: 10,
+              spreadRadius: 10,
+            ),
+
+          ],
         ),
+        margin: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+        // color: Colors.red.shade200,
+        child: Center(
+            child: Container(
+                color: Colors.yellow, child: Text('Hello Container'))),
       ),
     );
   }
