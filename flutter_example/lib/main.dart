fced5f7275c174ce59e6c913cf939cfb948e479d
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2024-03-21 00:36:27 +0900

    Buttons

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Body(),
+        body: SafeArea(child: Body()),
       ),
     ),
   );
@@ -16,94 +16,183 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
-        ExampleStateless(),
-        ExampleStateful(paramIndex : 3),
+        TestCheckBox(),
+        TestRadioButton(),
+        TestSlider(),
+        TestSwitch(),
       ],
     );
   }
 }
 
-class ExampleStateless extends StatelessWidget {
-  const ExampleStateless({super.key});
+///---------------------------Check Box
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckBoxState();
+}
+
+class _TestCheckBoxState extends State<TestCheckBox> {
+  late List<bool> values;
+
+  @override
+  void initState() {
+    super.initState();
+    values = [false, false, false];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.red, 
-      ),
+    return Row(
+      children: [
+        Checkbox(
+            value: values[0],
+            onChanged: (value) => changeValue(0, value: value)),
+        Checkbox(
+            value: values[1],
+            onChanged: (value) => changeValue(1, value: value)),
+        Checkbox(
+            value: values[2],
+            onChanged: (value) => changeValue(2, value: value)),
+      ],
+    );
+  }
+
+  void changeValue(int index, {bool? value = false}) {
+    //{bool? value = false} named 파라미터로 지정하는 방법
+    setState(() {
+      values[index] = value!;
+    });
+  }
+}
+
+///---------------------------Radio button
+class TestRadioButton extends StatefulWidget {
+  const TestRadioButton({super.key});
+
+  @override
+  State<TestRadioButton> createState() => _TestRadioButtonState();
+}
+
+enum TestRadioValue {
+  test1,
+  test2,
+  test3;
+}
+
+class _TestRadioButtonState extends State<TestRadioButton> {
+  TestRadioValue? selectValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Radio<TestRadioValue>(
+            value: TestRadioValue.test1,
+            groupValue: selectValue,
+            onChanged: (value) =>
+                setState(() => selectValue = value!), // 화살표 함수로 변경가능
+          ),
+          title: Text(TestRadioValue.test1.name),
+          onTap: () => setState(() {
+            if (selectValue != TestRadioValue.test1) {
+              selectValue = TestRadioValue.test1;
+            }
+          }),
+        ),
+        Radio<TestRadioValue>(
+          value: TestRadioValue.test2,
+          groupValue: selectValue,
+          onChanged: (value) => setState(() {
+            selectValue = value!;
+          }),
+        ),
+        Radio<TestRadioValue>(
+          value: TestRadioValue.test3,
+          groupValue: selectValue,
+          onChanged: (value) => setState(() {
+            selectValue = value!;
+          }),
+        ),
+      ],
     );
   }
 }
 
-class ExampleStateful extends StatefulWidget {
-  final int paramIndex;
-  const ExampleStateful({required this.paramIndex,super.key});
+///---------------------------Slider
+class TestSlider extends StatefulWidget {
+  const TestSlider({super.key});
 
   @override
-  State<ExampleStateful> createState() => _ExampleStatefulState();
+  State<TestSlider> createState() => _TestSliderState();
 }
 
-class _ExampleStatefulState extends State<ExampleStateful> {
-  ///모든 변수들은 StatefulState 상태객체 안에 넣어줘야 함.
-  /// setState 함수는 Widget에 상태가 바뀜을 알려주는 함수이다.(Widget rebuild)
-  late int index;
-  late int _index;
-  late TextEditingController textController;
-  ///late int 와 int? 차이는
-  ///late의 경우 나중에 무조건 값이 들어가야 함. 들어가지 않을 경우 에러 발생시킬 수 있음
-  ///?의 경우 나중에 값이 들어가지 않아도 됨.
-  int? sameIndex;
+class _TestSliderState extends State<TestSlider> {
+  late double value;
 
-  ///ctrl + o 누르면 자동 생성 가능
-  ///초기값 설정하는 방법
-  ///순서 중요 ! super.initState() 먼저 해줘야 함.
   @override
   void initState() {
-    super.initState(); //해당 함수를 사용해줘야 현재 위치의 widget의 상위 state도 initState가 됨
-    index = 5; //초기값 설정하는 방법
-
-    //(_변수 private 변수) _index 이런식으로 사용하는 것이 일반적임. widget.paramIndex 이렇게 다이렉트로 쓰는 것은 일반적이지 않음.
-    //stateful에서 변수 가져오는 방법.
-    _index = widget.paramIndex;
-
-    textController = TextEditingController();
-  }
-
-  ///dispose - Stream, 통신, ScrollController, TextController 사용시 사용의 끝을 알려줘야 리소스를 반납해야 함.
-  ///참고 - 변수 값은 것들은 자연스럽게 리소스 반납하고 사라짐.
-  ///순서 중요 ! super.initState() 나중에 해줘야 함.
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
+    super.initState();
+    value = 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            if (index == 5) {
-              index = 0;
-              return;
-            }
-            index++;
-          });
-        },
-        child: Container(
-          color: Colors.blue.withOpacity(index/5),
-          child: Center(
-            child: Text(
-              '${index}',
-              style: TextStyle(fontSize: 30),
-            ),
-          ),
+    // value 는 double값을 넣어줘야하는데 나는 꼭 int를 넣고싶다. -> value : value as double 이렇게 사용하면 된다고 함. as에 대한 공부필요할듯
+    return Column(
+      children: [
+        Text('${value.round()}'),
+        Slider(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+          divisions: 100,
+          max: 100,
+          min: 0,
+          label: value.round().toString(),
+          activeColor: Colors.red,
         ),
-      ),
+      ],
+    );
+  }
+}
+
+///---------------------------Switch
+class TestSwitch extends StatefulWidget {
+  const TestSwitch({super.key});
+
+  @override
+  State<TestSwitch> createState() => _TestSwitchState();
+}
+
+class _TestSwitchState extends State<TestSwitch> {
+  late bool value1;
+  late bool value2;
+
+  @override
+  void initState() {
+    super.initState();
+    value1 = false;
+    value2 = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Switch(
+          value: value1,
+          onChanged: (newValue) => setState(() => value1 = newValue),
+        ),
+        CupertinoSwitch(
+          value: value2,
+          onChanged: (newValue) => setState(() => value2 = newValue),
+          // CupertinoSwitch는 Theme에서 팁 변경도 가능하다고 함
+        ),
+      ],
     );
   }
 }
---
+++ flutter_example/lib/test/main8.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Body(),
+      ),
+    ),
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(paramIndex : 3),
+      ],
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red, 
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int paramIndex;
+  const ExampleStateful({required this.paramIndex,super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  ///모든 변수들은 StatefulState 상태객체 안에 넣어줘야 함.
+  /// setState 함수는 Widget에 상태가 바뀜을 알려주는 함수이다.(Widget rebuild)
+  late int index;
+  late int _index;
+  late TextEditingController textController;
+  ///late int 와 int? 차이는
+  ///late의 경우 나중에 무조건 값이 들어가야 함. 들어가지 않을 경우 에러 발생시킬 수 있음
+  ///?의 경우 나중에 값이 들어가지 않아도 됨.
+  int? sameIndex;
+
+  ///ctrl + o 누르면 자동 생성 가능
+  ///초기값 설정하는 방법
+  ///순서 중요 ! super.initState() 먼저 해줘야 함.
+  @override
+  void initState() {
+    super.initState(); //해당 함수를 사용해줘야 현재 위치의 widget의 상위 state도 initState가 됨
+    index = 5; //초기값 설정하는 방법
+
+    //(_변수 private 변수) _index 이런식으로 사용하는 것이 일반적임. widget.paramIndex 이렇게 다이렉트로 쓰는 것은 일반적이지 않음.
+    //stateful에서 변수 가져오는 방법.
+    _index = widget.paramIndex;
+
+    textController = TextEditingController();
+  }
+
+  ///dispose - Stream, 통신, ScrollController, TextController 사용시 사용의 끝을 알려줘야 리소스를 반납해야 함.
+  ///참고 - 변수 값은 것들은 자연스럽게 리소스 반납하고 사라짐.
+  ///순서 중요 ! super.initState() 나중에 해줘야 함.
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (index == 5) {
+              index = 0;
+              return;
+            }
+            index++;
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(index/5),
+          child: Center(
+            child: Text(
+              '${index}',
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
